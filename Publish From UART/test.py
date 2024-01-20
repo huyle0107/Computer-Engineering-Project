476e679c7d07f966f4be133842aab5f194ed3ac5
huyle0107 <legiahuy01072001@gmail.com>
2024-01-20 11:42:37 +0700

    change

--- Publish From UART/test.py
+++ Publish From UART/test.py
@@ -70,8 +70,8 @@ def get_all_values():
 
 root = tk.Tk()
 # Set the icon using PhotoImage
-# icon = PhotoImage(file="~/Desktop/MDT-128/Computer-Engineering-Project/icon_app.png")
-# root.tk.call('wm', 'iconphoto', root._w, icon)
+icon = PhotoImage(file="~/Desktop/MDT-128/Computer-Engineering-Project/icon_app.png")
+root.tk.call('wm', 'iconphoto', root._w, icon)
 
 # Bind the F11 key to toggle full-screen
 root.bind("<F11>", toggle_fullscreen)
